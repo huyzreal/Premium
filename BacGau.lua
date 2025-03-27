2eb43ef5ae2b23a8b1b9884940b948d4e9762883
huyzreal <phucnguyen44444dz@gmail.com>
2025-03-27 17:08:14 +0700

    BacGau.lua

--- BacGau.lua
+++ BacGau.lua
@@ -96,7 +96,7 @@ local v7 = CreateObject("ImageLabel", {
     Name = "UserImage",
     Parent = v6,
     BackgroundTransparency = 1,
-    Image = "rbxassetid://90008731814141",
+    Image = "rbxassetid://72380238122851",
     Position = UDim2.new(0, 15, 0, 10),
     Size = UDim2.new(0, 50, 0, 50)
 });
@@ -2633,7 +2633,7 @@ v23.BorderSizePixel = 0;
 v23.Position = UDim2.new(0.120833337 - 0.1, 0, 0.0952890813 + 0.01, 0);
 v23.Size = UDim2.new(0, 50, 0, 50);
 v23.Draggable = true;
-v23.Image = "http://www.roblox.com/asset/?id=90008731814141";
+v23.Image = "http://www.roblox.com/asset/?id=72380238122851";
 v24.Parent = v23;
 v24.CornerRadius = UDim.new(0, 12);
 v25.Parent = v23;
@@ -9325,6 +9325,6 @@ spawn(function()
 end);
 v14:Notify({
     Title = "BacGau Hub",
-    Content = "Loading...",
+    Content = "Cặc Ba Mày Đang Tải...",
     Duration = 10
 });
